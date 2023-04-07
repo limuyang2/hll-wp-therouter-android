ac14199c12bb915d49a24aef0941569f12650646
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2023-04-07 21:57:32 +0800

    支持KSP/AGP8，同步1.1.3-rc5内容

--- plugin-agp8/src/main/groovy/com/therouter/plugin/BuildConfig.java
+++ plugin-agp8/src/main/groovy/com/therouter/plugin/BuildConfig.java
@@ -1,8 +1,8 @@
-package com.therouter.apt;
+package com.therouter.plugin;
 
 public final class BuildConfig {
     public static final String VERSION = "1.1.4-beta1";
-    public static final String NAME = "apt";
+    public static final String NAME = "plugin-agp8";
 
     private BuildConfig() {
     }
--- router/src/main/java/com/therouter/BuildConfig.java
+++ router/src/main/java/com/therouter/BuildConfig.java
@@ -1,8 +1,8 @@
-package com.therouter.apt;
+package com.therouter;
 
 public final class BuildConfig {
     public static final String VERSION = "1.1.4-beta1";
-    public static final String NAME = "apt";
+    public static final String NAME = "router";
 
     private BuildConfig() {
     }
